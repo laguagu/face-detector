d7d1651daaf0418d14537a2588eab1ce2bf8078c
laguagu <49702497+laguagu@users.noreply.github.com>
2024-02-11 13:43:37 +0200

    latest things

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          {children}
+          <Toaster/>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
